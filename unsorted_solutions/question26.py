58dd8c771673e8e61cb0543a3496bfab3d7e8217
neequole <nicole.tibay@johngaltventures.com>
2017-10-03 13:27:24 +0800

    Solutions for questions set 26-27

---
+++ unsorted_solutions/question26.py
@@ -0,0 +1,15 @@
+"""  Question:
+Define a function which can compute the sum of two numbers.
+
+Hints:
+Define a function with two numbers as arguments.
+You can compute the sum in the function and return the value.
+"""
+
+
+def sum(a, b):
+    return a + b
+
+print(sum(1, 2))
+print(sum(3, 4))
+print(sum(5, 6))
---
+++ unsorted_solutions/question27.py
@@ -0,0 +1,15 @@
+"""  Question:
+Define a function that can convert a integer into a string and print it in console.
+
+Hints:
+Use str() to convert a number to string.
+"""
+
+
+def to_string(num):
+    print(str(num))
+
+
+to_string(1)
+to_string(11)
+to_string(15)
