3496edcf338bb884f1b0d6be1ea7971f5f526252
neequole <nicole.tibay@johngaltventures.com>
2017-10-04 21:10:53 +0800

    Solutions to question set 36-40

---
+++ unsorted_solutions/question36.py
@@ -0,0 +1,20 @@
+"""  Question 36:
+Define a function which can generate and print a list where the values are
+square of numbers between 1 and 20 (both included).
+
+Hints:
+
+Use ** operator to get power of a number.
+Use range() for loops.
+Use list.append() to add values into a list.
+"""
+
+
+def foo():
+    out = []
+    for i in range(1, 21):
+        out.append(i ** 2)
+    print(out)
+
+
+foo()
